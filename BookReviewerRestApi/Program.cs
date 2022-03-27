9092297224f8d634aac24e1c4ee09940358c6b5b
liitlelenim <jakubblaszczyk20@gmail.com>
2022-03-27 14:37:11 +0200

    add db context and temporary in memory database

--- BookReviewerRestApi/Program.cs
+++ BookReviewerRestApi/Program.cs
@@ -1,7 +1,10 @@
+using BookReviewerRestApi;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("temporaryTestDatabase"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
