21623a7fe54f751b74c36ae88c4373f0c72651a5
liitlelenim <jakubblaszczyk20@gmail.com>
2022-03-29 17:55:35 +0200

    add book proposal entity

---
+++ BookReviewerRestApi/Entities/BookProposal.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookReviewerRestApi.Entities
+{
+    public class BookProposal
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string BookTitle { get; set; } = String.Empty;
+        public string BookAuthorFullName { get; set; } = String.Empty;
+        public string BookDescription { get; set; } = String.Empty;
+        public string BookCoverUrl { get; set; } = String.Empty;
+        [Required]
+        public BookProposalStatus Status { get; set; } = BookProposalStatus.Pending;
+        [Required]
+        public AppUser ProposedBy { get; set; }
+
+
+    }
+
+    public enum BookProposalStatus
+    {
+        Pending,
+        Rejected,
+        Accepted
+    }
+}
